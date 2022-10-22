b4799f6f00a0e27662cf7aea890cecd562edd3b5
Sushant <sushantgangwar87@gmail.com>
2022-10-22 10:38:17 +0530

    Fixed Files

--- pages/api/user/all.js
+++
@@ -1,9 +0,0 @@
-import User from "../../database/models/userSchema";
-import connection from '../../database/connection'
-
-export default async function handler(req, res) {
-    connection();
-    const users = await User.find()
-
-    res.status(200).json({success:true,users})
-}
